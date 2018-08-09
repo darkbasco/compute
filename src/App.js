96f9c07557ea866ecf320d91cc4bde7093da216e
waw01 <bill_werner@homedepot.com>
2018-08-08 23:37:50 -0400

    all ops

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ import oldguy from "./comp_oldguy.png"
 import deadlift from "./comp_deadlift.png"
 import geek from "./comp_geek.png"
 import dumbellsilver from "./dumbell_silver.png"
+import plus from "./sign_plus.png"
+import minus from "./sign_minus.png"
+import mult from "./sign_mult.png"
+import divi from "./sign_divi.png"
+import equal from "./sign_equal.png"
 import "./App.css"
 
 class Problem {
@@ -17,6 +22,7 @@ class Problem {
         this.problem = problem
         this.answer = answer
         this.correct = correct
+        console.log("PROBLEM CONSTRUCT", problem, answer, correct)
     }
 }
 
@@ -24,11 +30,18 @@ class App extends Component {
     constructor() {
         super()
 
-        const first = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-        const second = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        const first = [0, 1]
+        const second = [0, 1]
+        // const first = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        // const second = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
         this.state = {
-            probSet: this.generateProblemSet(first, second, ["+", "-"]),
+            probSet: this.generateProblemSet(first, second, [
+                "plus",
+                "minus",
+                "mult",
+                "divi"
+            ]),
             keys: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
             currentProblem: null,
             currentAnswer: null,
@@ -36,34 +49,62 @@ class App extends Component {
             notification: false,
             history: []
         }
+        console.log("APP CONSTRUCT", this.state)
     }
 
-
-    generateProblemSet = (first, second, third) => 
+    generateProblemSet = (first, second, third) =>
         first.reduce((acc_a, curr_a) => {
             return acc_a.concat(
                 second.reduce((acc_b, curr_b) => {
                     return acc_b.concat(
                         third.reduce((acc_c, curr_c) => {
-                            if(curr_c === "+"){
+                            if (curr_c === "plus") {
                                 return acc_c.concat({ curr_a, curr_b, curr_c })
-                            } else if (curr_c === "-") {
-                                if(curr_b <= curr_a) return acc_c.concat({ curr_a, curr_b, curr_c })
+                            } else if (curr_c === "minus") {
+                                if (curr_b <= curr_a) {
+                                    return acc_c.concat({
+                                        curr_a,
+                                        curr_b,
+                                        curr_c
+                                    })
+                                } else {
+                                    return acc_c
+                                }
+                            } else if (curr_c === "mult") {
+                                return acc_c.concat({ curr_a, curr_b, curr_c })
+                            } else if (curr_c === "divi") {
+                                if (
+                                    curr_b <= curr_a &&
+                                    curr_a % curr_b === 0 &&
+                                    !(curr_a === 0 && curr_b === 0)
+                                ) {
+                                    return acc_c.concat({
+                                        curr_a,
+                                        curr_b,
+                                        curr_c
+                                    })
+                                } else {
+                                    return acc_c
+                                }
+                            } else {
+                                return acc_c
                             }
-                    },[])
-                )
+                        }, [])
+                    )
                 }, [])
             )
         }, [])
-    
 
     getInitialAnswer = () => "?"
 
     shuffleCurrentProblem = answeredPrevious => {
         const probIndex = Math.floor(Math.random() * this.state.probSet.length)
+        console.log("shuffleCurrentProblem", probIndex, answeredPrevious)
 
         this.setState(prevState => ({
-            currentProblem: prevState.probSet[probIndex],
+            currentProblem: prevState.probSet[probIndex]
+                ? prevState.probSet[probIndex]
+                : prevState.probSet[0],
             currentAnswer: null,
             currentIndex: probIndex,
             history:
@@ -78,7 +119,6 @@ class App extends Component {
                       ]
                     : [...prevState.history]
         }))
-        console.log(this.state.history)
     }
 
     removeCurrentProblem = () => {
@@ -113,6 +153,7 @@ class App extends Component {
     }
 
     buildAnswer = (prevAnswer, currAnswer) => {
+        console.log("buildAnswer", prevAnswer, currAnswer)
         if (prevAnswer >= 0) {
             return prevAnswer * 10 + currAnswer
         } else {
@@ -121,29 +162,42 @@ class App extends Component {
     }
 
     checkAnswer = (problem, answer) => {
-        console.log("CHECK", problem, answer)
-        if (problem.curr_c === "+" && problem.curr_a + problem.curr_b === answer) {
+        console.log("checkAnswer", problem, answer)
+        if (
+            problem.curr_c === "plus" &&
+            problem.curr_a + problem.curr_b === answer
+        ) {
             this.removeCurrentProblem()
             this.shuffleCurrentProblem(true)
             return true
-        } else if (problem.curr_c === "-" && problem.curr_a - problem.curr_b === answer) {
+        } else if (
+            problem.curr_c === "minus" &&
+            problem.curr_a - problem.curr_b === answer
+        ) {
             this.removeCurrentProblem()
             this.shuffleCurrentProblem(true)
             return true
-        // } else if (problem.curr_c === "x" && problem.curr_a + problem.curr_b === answer) {
-        //     this.removeCurrentProblem()
-        //     this.shuffleCurrentProblem(true)
-        //     return true
-        // } else if (problem.curr_c === "/" && problem.curr_a + problem.curr_b === answer) {
-        //     this.removeCurrentProblem()
-        //     this.shuffleCurrentProblem(true)
-        //     return true
+        } else if (
+            problem.curr_c === "mult" &&
+            problem.curr_a * problem.curr_b === answer
+        ) {
+            this.removeCurrentProblem()
+            this.shuffleCurrentProblem(true)
+            return true
+        } else if (
+            problem.curr_c === "divi" &&
+            problem.curr_a / problem.curr_b === answer
+        ) {
+            this.removeCurrentProblem()
+            this.shuffleCurrentProblem(true)
+            return true
         } else {
             return false
         }
     }
 
     clearAnswer = () => {
+        console.log("clearAnswer")
         this.setState(() => ({
             currentAnswer: null,
             notification: null
@@ -151,6 +205,7 @@ class App extends Component {
     }
 
     getBadge = () => {
+        console.log("getBadge")
         let level = 0
         for (
             let index = this.state.history.length - 1;
@@ -189,6 +244,7 @@ class App extends Component {
     }
 
     render() {
+        console.log("RENDER APP")
         return (
             <div className="App">
                 <div className="TitleBar">
@@ -202,34 +258,51 @@ class App extends Component {
                 <div className="ScoreBar">
                     Reps Remaining: {this.state.probSet.length}
                 </div>
-
-                <Game
-                    probs={this.state.probSet}
-                    answerTimer={this.answerTimer}
-                    currentProblem={this.state.currentProblem}
-                    currentAnswer={this.state.currentAnswer}
-                    currentOperator={this.state.currentOperator}
-                    removeCurrentProblem={this.removeCurrentProblem}
-                    shuffleCurrentProblem={this.shuffleCurrentProblem}
-                    notification={this.state.notification}
-                    getInitialAnswer={this.getInitialAnswer}
-                    history={this.state.history}
-                />
-
-                <div className="keyBoardContainer">
-                    <KeyBoard
-                        keys={this.state.keys}
-                        updateAnswer={this.updateAnswer}
-                    />
-                    <button
-                        className="ClearButton"
-                        onClick={() => {
-                            this.clearAnswer()
-                        }}
-                    >
-                        Clear
-                    </button>
-                </div>
+                {this.state.probSet.length > 0 ? (
+                    <div>
+                        <Game
+                            probs={this.state.probSet}
+                            answerTimer={this.answerTimer}
+                            currentProblem={this.state.currentProblem}
+                            currentAnswer={this.state.currentAnswer}
+                            currentOperator={this.state.currentOperator}
+                            removeCurrentProblem={this.removeCurrentProblem}
+                            shuffleCurrentProblem={this.shuffleCurrentProblem}
+                            notification={this.state.notification}
+                            getInitialAnswer={this.getInitialAnswer}
+                            history={this.state.history}
+                            // state={this.state}
+                        />
+                        <div className="keyBoardContainer">
+                            <KeyBoard
+                                keys={this.state.keys}
+                                updateAnswer={this.updateAnswer}
+                            />
+                            <button
+                                className="ClearButton"
+                                onClick={() => {
+                                    this.clearAnswer()
+                                }}
+                            >
+                                Clear
+                            </button>
+                        </div>
+                    </div>
+                ) : (
+                    <div className="TheEnd">
+                        <div>GREAT JOB!!!</div>
+                        <div>
+                            <button
+                                className="ClearButton"
+                                onClick={() => {
+                                    this.clearAnswer()
+                                }}
+                            >
+                                Restart
+                            </button>
+                        </div>
+                    </div>
+                )}
             </div>
         )
     }
@@ -239,27 +312,51 @@ class Game extends React.Component {
     constructor(props) {
         super(props)
         this.props.shuffleCurrentProblem(false)
+        console.log("CONSTRUCT GAME")
+    }
+
+    getSymbol(name) {
+        if (name === "plus") {
+            return <img src={plus} className="ProbSymb" alt="Correct" />
+        } else if (name === "minus") {
+            return <img src={minus} className="ProbSymb" alt="Correct" />
+        } else if (name === "mult") {
+            return <img src={mult} className="ProbMult" alt="Correct" />
+        } else if (name === "divi") {
+            return <img src={divi} className="ProbSymb" alt="Correct" />
+        } else {
+            return
+        }
     }
 
     render() {
         console.log("RENDER GAME")
-        console.log("RENDER", this.props.currentProblem, this.props.currentAnswer)
+        console.log("RENDER", this.props.state)
         return (
             <div className="ProbAll">
-                {this.props.currentProblem !== null ? (
+                {this.props.currentProblem !== null &&
+                typeof this.props.currentProblem !== "undefined" ? (
                     <div className="ProbSpace">
                         <div className="ProbNum">
                             {this.props.currentProblem !== null
                                 ? this.props.currentProblem.curr_a
                                 : 0}
                         </div>
-                        <div className="ProbSymb">{this.props.currentProblem.curr_c}</div>
+                        <div className="ProbSymb">
+                            {this.getSymbol(this.props.currentProblem.curr_c)}
+                        </div>
                         <div className="ProbNum">
                             {this.props.currentProblem !== null
                                 ? this.props.currentProblem.curr_b
                                 : 0}
                         </div>
-                        <div className="ProbSymb">=</div>
+                        <div className="ProbSymb">
+                            <img
+                                src={equal}
+                                className="ProbSymb"
+                                alt="Correct"
+                            />
+                        </div>
                         <div className="ProbNum">
                             {this.props.currentAnswer !== null
                                 ? this.props.currentAnswer
@@ -283,9 +380,12 @@ class Game extends React.Component {
 class KeyBoard extends React.Component {
     constructor(props) {
         super(props)
+        console.log("CONSTRUCT KEYB")
     }
 
     render() {
+        console.log("RENDER KEYB")
+
         return (
             <div className="KeyBoard">
                 {this.props.keys.map(k => (
